b4133098343d3faddfa2a99a1d99fd706060ed7e
Mohammad1724 <129998091+Mohammad1724@users.noreply.github.com>
2026-07-17 11:08:49 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ RED='\033[0;31m'; GREEN='\033[0;32m'; BLUE='\033[0;34m'; CYAN='\033[0;36m'; NC='
 
 echo -e "${CYAN}╔══════════════════════════════════════════════╗${NC}"
 echo -e "${CYAN}║      MRM Manager Installer v1.0.4                         ║${NC}"
-echo -e "${CYAN}║                   ║${NC}"
+echo -e "${CYAN}║      ║${NC}"
 echo -e "${CYAN}╚══════════════════════════════════════════════╝${NC}"
 echo ""
 
